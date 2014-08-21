5514bcbeb6e0e47e67b30b8bd349f73a4b4e510b
Gesnet <gesilliya0@gmail.com>
2014-08-21 19:04:15 +0900

    JS 기능 추가
    1. 화면 width 사이즈 조정
    2. .back 뒤에 추가적으로 classname이 있는 경우에 background-image 추가
    
    수정해야 할 것
    1. 맨 처음 로딩 시의 .front 의 height 값

--- jpage.js
+++ jpage.js
@@ -6,8 +6,54 @@ var count;
 
 /* #main #work_view li 의 최초 가로값 자동지정
 *****************************************************************/
-$("#main .works_view > li").css("min-width", (w/3)-2+"px");
+$("#main .works_view > li").css("width", (w/3)-2+"px");
 
-/* #main #work_view li 내 아이콘 값 변경
+/* 화면 리사이징 시 #main .works_view li 의 가로값 자동 설정
 *****************************************************************/
-// var icons = $("#main .works_view .front > p").text().split(' ');
+$(window).on("resize", function(){
+	 w = $(this).width();
+	 $("#main .works_view > li").css("clear", "none");
+	 if(w>1125){
+		$("#main .works_view > li").css("width", (w/3)-2+"px");
+		$("#main .works_view > li:nth-child(3n+1)").css("clear", "both");
+	}else if(w>774){
+		$("#main .works_view > li").css("width", (w/2)-1+"px");
+		$("#main .works_view > li:nth-child(2n+1)").css("clear", "both");
+	}else{
+		 $("#main .works_view > li").css("width", "100%");
+		 $("#main .works_view > li").css("clear", "both");
+	}
+});
+
+/* #main #work_view li 내 아이콘 값 변경 (text>img)
+*****************************************************************/
+var $icons = $("#main .works_view .front > p.icon");
+
+$.each($icons, function(index, el) {
+	var $el = $(el),
+		el_text = $el.text();
+	if ( el_text.indexOf('pc') > -1 ) {
+		$icons.eq(index).append("<span style='float: right;'><img src='img/icons/icon_web.gif'></span>");
+	}
+	if ( el_text.indexOf('tablet') > -1 ) {
+		$icons.eq(index).append("<span  style='float: right;'><img src='img/icons/icon_pad.gif'></span>");
+	}
+	if ( el_text.indexOf('phone') > -1 ) {
+		$icons.eq(index).append("<span  style='float: right;'><img src='img/icons/icon_phone.gif'></span>");
+	}
+});
+
+$icons.css("color", "#eaeaea");
+
+/* div.back 에 다른 클래스명이 있는 경우, 해당 클래스명이 들어간 visual_.gif 를 background로 넣음.
+*****************************************************************/
+var $visual = $("#main .works_view .back");
+
+for(var i = 0; i < $visual.length; i++){
+	if($visual[i].classList.length > 1){
+		$($visual[i]).css("background-image", "url('img/customers_visual/"+$visual[i].classList[1]+"_visual.jpg')");
+		// $(this).css("background-image", "url('img/customers_visual/"+$visual[i].classList[1]+"_visual.jpg')");
+	}
+}
+
+// $("#main .works_view li").mouseenter();
